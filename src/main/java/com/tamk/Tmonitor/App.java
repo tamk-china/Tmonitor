86af38a668c39ad7ceb503971eeb9d28ceef4f05
kuanqiang.tkq <kuanqiang.tkq@alibaba-inc.com>
2016-05-04 22:51:39 +0800

    Tmonitor

---
+++ src/main/java/com/tamk/Tmonitor/App.java
@@ -0,0 +1,13 @@
+package com.tamk.Tmonitor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
